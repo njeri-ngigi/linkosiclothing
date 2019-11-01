0732f5a35741f05e5965dfabefccd4616aeda97d
njeri-ngigi <njery.ngigi@gmail.com>
2019-11-01 21:22:17 +0200

    redesign header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,40 @@
 import React from "react";
-import "../styles/navbar.scss";
-import Navbar from "./navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import "../styles/header.scss";
+
 
 const header = () => (
   <div className="header">
-    <Navbar/>
-    <div className="bg-image"></div>
-    <div className="collections">
-      <h1 className="collection-type">URBAN COLLECTION</h1>
-      <button>SHOP</button>
+    <div className="header__social--icons">
+      <span><FontAwesomeIcon icon={faInstagram}/></span>
+      <span><FontAwesomeIcon icon={faTwitter}/></span>
+      <span><FontAwesomeIcon icon={faFacebook}/></span>
+    </div>
+
+    <div className="header__intro">
+      <span className="header__span--abs"></span>
+      <h1>Links & Clo</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
+      </p>
+      <div class="header__intro--div--arrow">
+        <p class="header__intro--arrow"></p>
+      </div>
+      <button className="header__intro--whats--new">What's new</button>
+    </div>
+
+    <div className="header__images">
+      <div className="header__images--1">
+        <img src="images/linkosiclothing/black-dress.jpg" alt="urban camo"/>
+      </div>
+      <div className="header__images--2">
+        <img src="images/linkosiclothing/camo-3.jpg" alt="urban camo"/>
+      </div>
+      <div className="header__images--3">
+        <img src="images/linkosiclothing/sweats.jpg" alt="urban camo"/>
+      </div>
     </div>
   </div>
 );
