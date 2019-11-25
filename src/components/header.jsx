1ce01b13dd9c3b27759d2c8b9ceb67545fa1fda4
njeri-ngigi <njery.ngigi@gmail.com>
2019-11-25 10:59:20 +0200

    redesign 1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,15 @@ import "../styles/header.scss";
 
 
 const header = () => (
+  <>
+  {/* <div className="pulsing-header">
+    <img src="/images/linkosiclothing/camo.jpg"/>
+  </div> */}
+  {/* <div className="pulsing-header-2"></div>
+  <div className="pulsing-header-3"></div>
+  <div className="pulsing-header-4"></div>
+  <div className="pulsing-header-5"></div> */}
+
   <div className="header">
     <div className="header__social--icons">
       <span><FontAwesomeIcon icon={faInstagram}/></span>
@@ -15,9 +24,9 @@ const header = () => (
 
     <div className="header__intro">
       <span className="header__span--abs"></span>
-      <h1>Links & Clo</h1>
+      <h1>L & C</h1>
       <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
+        Welcome 
       </p>
       <div className="header__intro--div--arrow">
         <p className="header__intro--arrow"></p>
@@ -29,14 +38,17 @@ const header = () => (
       <div className="header__images--1">
         <img src="images/linkosiclothing/black-dress.jpg" alt="urban camo"/>
       </div>
+      
       <div className="header__images--2">
         <img src="images/linkosiclothing/camo-3.jpg" alt="urban camo"/>
       </div>
+      
       <div className="header__images--3">
         <img src="images/linkosiclothing/sweats.jpg" alt="urban camo"/>
       </div>
     </div>
   </div>
+  </>
 );
 
 export default header;
